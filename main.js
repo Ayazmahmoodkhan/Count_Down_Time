e866a9cb69a826b16f20d56111537438d1ba61e1
Ayazmahmoodkhan <94978162+Ayazmahmoodkhan@users.noreply.github.com>
2022-08-31 01:51:21 +0500

    initial
    initial

---
+++ main.js
@@ -0,0 +1,19 @@
+const endDate = "01 junuary 2023 12:00 AM";
+
+document.getElementById("endDate").innerHTML = endDate;
+const inputs = document.querySelectorAll("input") ;
+const  clock =()=>{
+const end = new Date(endDate);
+const now = new Date();
+const diff = (end - now)/ 1000
+if (diff<0) return;    
+inputs[0].value = Math.floor(diff/ 3600 /24);
+inputs[1].value = Math.floor(diff/ 3600) % 24;
+inputs[2].value = Math.floor(diff/ 60)%60;
+inputs[3].value = Math.floor(diff)%60;
+}
+ setInterval(
+    () =>{
+       clock()
+    },1000
+ );
