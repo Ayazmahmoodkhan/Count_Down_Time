5cd9edcde9faef523928b9772b1979215eccfd89
Ayazmahmoodkhan <94978162+Ayazmahmoodkhan@users.noreply.github.com>
2022-09-05 21:44:21 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
-const endDate = "01 junuary 2023 12:00 AM";
-
+// Given from user
+const endDate = prompt("Enter Date and  Time \r\n 01 junuary 2023 12:00 AM");
+//  other wise set bydefault value::
+// const endDate = "01 junuary 2023 12:00 AM";
 document.getElementById("endDate").innerHTML = endDate;
-const inputs = document.querySelectorAll("input") ;
-const  clock =()=>{
-const end = new Date(endDate);
-const now = new Date();
-const diff = (end - now)/ 1000
-if (diff<0) return;    
-inputs[0].value = Math.floor(diff/ 3600 /24);
-inputs[1].value = Math.floor(diff/ 3600) % 24;
-inputs[2].value = Math.floor(diff/ 60)%60;
-inputs[3].value = Math.floor(diff)%60;
+const inputs = document.querySelectorAll("input");
+const clock = () => {
+    const end = new Date(endDate);
+    const now = new Date();
+    const diff = (end - now) / 1000
+    if (diff < 0) 
+        return;
+    
+    inputs[0].value = Math.floor(diff / 3600 / 24);
+    inputs[1].value = Math.floor(diff / 3600) % 24;
+    inputs[2].value = Math.floor(diff / 60) % 60;
+    inputs[3].value = Math.floor(diff) % 60;
 }
- setInterval(
-    () =>{
-       clock()
-    },1000
- );
+setInterval(() => {
+    clock()
+}, 1000);
